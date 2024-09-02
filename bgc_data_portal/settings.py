9dcb58d1014b84e1bfe78915673a9b272ec5d401
Santiago Sanchez <santiago.s.fragoso@gmail.com>
2024-09-02 23:09:21 +0100

    k8s files

--- bgc_data_portal/settings.py
+++ bgc_data_portal/settings.py
@@ -16,11 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG_DB = os.getenv('DEBUG_DB')
-DATABASE_NAME = os.getenv(
-    'PROD_DATABASE' if DEBUG_DB.lower()=='false' else 'DEV_DATABASE',
-    f"sqlite:///{BASE_DIR}/bgc_database.db.debug.db"
-    )
+DEBUG_DB = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
